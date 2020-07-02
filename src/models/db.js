4f95ba77939ab074169437a95aff41ba10a16318
Muhammad-Moiz <muhammadmoiz2221@gmail.com>
2020-07-02 12:12:44 +0500

    update mongodb connections again

--- src/models/db.js
+++ src/models/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
  
  
-const CONNECTION_URI ="mongodb+srv://muhammad-moiz:moiz786110@fyp-49lay.mongodb.net/<dbname>?retryWrites=true&w=majority" || 'mongodb://localhost:27017/mydb';
+const CONNECTION_URI ="mongodb+srv://muhammad-moiz:moiz786110@fyp-49lay.mongodb.net/mydb?retryWrites=true&w=majority" || 'mongodb://localhost:27017/mydb';
 
 mongoose.connect( CONNECTION_URI , { useNewUrlParser: true }, (err) => {
     if (!err) { 
