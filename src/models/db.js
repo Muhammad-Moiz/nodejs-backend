f697d13d1f8fde64cafc58a886ba475a8719e381
Muhammad-Moiz <muhammadmoiz2221@gmail.com>
2020-07-02 11:18:34 +0500

    update database connection to connect mongodb with mongodb atlas

--- src/models/db.js
+++ src/models/db.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 
+const uri = "mongodb+srv://muhammad-moiz:moiz786110@fyp-49lay.mongodb.net/<dbname>?retryWrites=true&w=majority";
 
+ 
 const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/mydb';
 
-mongoose.connect( CONNECTION_URI , { useNewUrlParser: true }, (err) => {
+mongoose.connect( uri , { useNewUrlParser: true }, (err) => {
     if (!err) { 
         console.log('MongoDB Connection Succeeded.');
     }
     else { console.log('Error in DB connection : ' + err) }
 });
 
+ 
 require('./item');
 require('./user');
+
