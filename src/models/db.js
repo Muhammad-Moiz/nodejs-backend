6a5b1d2744c3309be98a91108c9c1edae40e4531
Muhammad-Moiz <muhammadmoiz2221@gmail.com>
2020-07-02 18:41:20 +0500

    update db connection string

--- src/models/db.js
+++ src/models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const CONNECTION_URI ="mongodb+srv://muhammad-moiz:moiz786110@fyp-49lay.mongodb.net/mydb?retryWrites=true&w=majority";
+const CONNECTION_URI ="mongodb+srv://muhammad-moiz:hhMwl6nRSWaEXbKs@fyp-49lay.mongodb.net/mydb?retryWrites=true&w=majority";
 
 mongoose.connect( CONNECTION_URI , { useNewUrlParser: true }, (err) => {
     if (!err) {
